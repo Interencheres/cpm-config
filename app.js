adb7d812cdfe30ef23ed8d30629fe7c5957b9e5e
Gregory Gerard <wako057@gmail.com>
2021-04-27 11:27:43 +0200

    fix(app): fix tests

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const process = require("process");
 module.exports = (() => {
     switch (process.env.NODE_ENV) {
         case "test":
-            return require("../../config-test.json");
-        /* istanbul ignore next */
+            return require("../../../config-test.json");
+        case "ci":
+            if (__dirname.indexOf("node_modules") === -1) {
+                return require("./config-test.json");
+            }
+        // eslint-disable-next-line no-fallthrough
         default:
-            return require("../../config.json");
+            return require("../../../config.json");
     }
 })();
